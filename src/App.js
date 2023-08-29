ffbfc26df51862bc3ee5688f71bbc10dfef94f0b
karmit1296 <karmit1lalani@gmail.com>
2023-08-29 01:17:19 -0400

    started food delivery project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
+const Header = () => {
+  return(<div className='header'>
+    <div className='logo-container'>
+      <img className='logo' src="https://tse3.mm.bing.net/th?id=OIP.-Yyvp72aoLShDLLKl_-K0AAAAA&pid=Api&P=0&h=180 " alt="" />
+    </div>
+    <div className='nav-items'>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+    </div>
+  </div>)
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      {Header()}
     </div>
   );
 }
