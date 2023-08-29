b61a9773beaaefff032d218656a574a22ffc3bb3
karmit1296 <karmit1lalani@gmail.com>
2023-08-29 14:50:31 -0400

    added structure of restraunt website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import './App.css';
+import "./App.css";
 
 const Header = () => {
-  return(<div className='header'>
-    <div className='logo-container'>
-      <img className='logo' src="https://tse3.mm.bing.net/th?id=OIP.-Yyvp72aoLShDLLKl_-K0AAAAA&pid=Api&P=0&h=180 " alt="" />
-    </div>
-    <div className='nav-items'>
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://tse3.mm.bing.net/th?id=OIP.-Yyvp72aoLShDLLKl_-K0AAAAA&pid=Api&P=0&h=180 "
+          alt=""
+        />
+      </div>
+      <div className="nav-items">
         <ul>
           <li>Home</li>
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
         </ul>
+      </div>
     </div>
-  </div>)
+  );
+};
+
+const RestrauntCard = (props) => {
+  const {resData} = props
+  return(
+    <div className='res-card' style={{
+      backgroundColor:'#f0f0f0'
+    }}>
+      <img src="https://www.kelvinfarrell.ie/img/chipshop-1.jpg" alt="res-logo" className='res-logo' />
+      <h3>{resData.name}</h3>
+      <h4>{resData.cusine}</h4>
+      <h4>{resData.stars}</h4>
+      <h4>{resData.time} minutes</h4>
+
+      </div>
+  )
 }
+
+const resObj={
+  name:'Cheese and Chips',
+  cusine:'American',
+  stars:'4.3',
+  time:'20'
+
+  
+}
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestrauntCard resData={resObj}/>
+
+      </div>
+    </div>
+  );
+};
+
 function App() {
   return (
-    <div className='app'>
-      {Header()}
+    <div className="app">
+      <Header />
+      <Body />
     </div>
   );
 }
