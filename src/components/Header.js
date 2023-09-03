902f9b22f1b832df90c6aa6cf92dd458134d48da
karmit1296 <karmit1lalani@gmail.com>
2023-09-03 01:53:57 -0400

    progressing on routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,41 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
 
-  const[btnName,setBtnName]= useState('Login')
-
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src="https://tse3.mm.bing.net/th?id=OIP.-Yyvp72aoLShDLLKl_-K0AAAAA&pid=Api&P=0&h=180 "
-            alt=""
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-            <button className="login" onClick={()=>{
-              btnName=='Login'?setBtnName('Logout'):setBtnName('Login')
-            }}>{btnName}</button>
-          </ul>
-        </div>
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://tse3.mm.bing.net/th?id=OIP.-Yyvp72aoLShDLLKl_-K0AAAAA&pid=Api&P=0&h=180 "
+          alt=""
+        />
       </div>
-    );
-  };
-  
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnName == "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-
-  export default Header;
+export default Header;
